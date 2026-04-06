861ae81a88027d41f04683d4fbc96f5e9ec37ac3
Eugene <eugene_dyl@Eugenes-MacBook-Pro.local>
2026-04-06 17:49:05 +0200

    swagger fix

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ app.add_middleware(
     allowed_hosts=["*"],
 )
 # Routers
-app.include_router(image_upload.router)
 app.include_router(google_auth.router)
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(dreams.router)
+app.include_router(image_upload.router)
 app.include_router(admin.router)
 app.include_router(password_reset.router)
 app.include_router(statistics.router)
